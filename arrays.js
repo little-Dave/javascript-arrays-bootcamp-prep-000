dd58bae9b3b8438034480813760e02735b8081ce
Dave Boehmer <daboehmer@gmail.com>
2019-04-04 16:16:46 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -1,7 +1,8 @@
 var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles']
 
 function addElementToBeginningOfArray(array, element) {
-  
+  var newArray = [...array, element];
+  return newArray;
 }
 
 function destructivelyAddElementToBeginningOfArray(array, element) {
